339efb5667f7b3441f9b20f8ed145a92d8e19a6e
DinukaD-24 <dinukailangakoon@gmail.com>
2025-12-24 16:14:02 +0530

    Remove Vite demo UI and initialize Librioo base app

---
+++ frontend/librioo-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Librioo – Smart Library Assistant System</h1>
+      <p>Member & Admin Web Interface</p>
+    </div>
+  );
+}
+
+export default App;
