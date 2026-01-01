d9c0fcb6ffc9e08245afc7b71b188c3643508175
DinukaD-24 <dinukailangakoon@gmail.com>
2026-01-02 01:58:57 +0530

    Refactor RobotLayout styles for improved responsiveness and adjust LoginPage structure

--- frontend/src/robot/layouts/RobotLayout.jsx
+++ frontend/src/robot/layouts/RobotLayout.jsx
@@ -5,10 +5,10 @@ export default function RobotLayout({ children }) {
     <main className="bg-[linear-gradient(180deg,#2c3e50_0%,#4a6278_100%)] w-full h-screen relative">
 
       {/* Header */}
-      <header className="absolute top-3.5 left-16 w-[1152px] h-[100px]">
-        <div className="absolute top-[10px] left-[100px] w-[1280px] h-[100px] bg-[#d9d9d959] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
+      <header className="relative w-full mt-4 h-[120px]">
+        <div className="absolute top-[10px] left-[100px] right-[100px] h-[100px] bg-[#d9d9d959] rounded-[20px] shadow-[0px_4px_4px_#00000040]" />
 
-        <h2 className="absolute top-[45px] left-[145px] [-webkit-text-fill-color:white] [font-family:'Aldrich-Regular',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] whitespace-nowrap">
+        <h2 className="absolute top-[50px] left-[145px] [-webkit-text-fill-color:white] [font-family:'Aldrich-Regular',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] whitespace-nowrap">
           Smart Library Assistant
         </h2>
 
@@ -20,7 +20,7 @@ export default function RobotLayout({ children }) {
       </header>
 
       {/* Page content */}
-      <section className="absolute top-[143px] left-[194px] w-[650px]">
+      <section className="top-[143px] left-[194px] right-[150px]" relative>
         {children}
       </section>
 
