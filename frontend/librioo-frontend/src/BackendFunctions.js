f2de8cd170c7a460731b87eb32844447adec116b
sandun80 <sandunsd123@gmail.com>
2026-01-01 08:54:05 +0530

    guestLogin implemented for backend

--- frontend/librioo-frontend/src/BackendFunctions.js
+++ frontend/librioo-frontend/src/BackendFunctions.js
@@ -31,13 +31,32 @@ export async function searchBooksByName(keyword) {
       `http://localhost:8080/api/searchname?keyword=${encodeURIComponent(keyword)}`
     );
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch books");
-    }
+    // if (!response.ok) {
+    //   throw new Error("Failed to fetch books");
+    // }
 
     return await response.json();
   } catch (error) {
     console.error("Search API error:", error);
     return [];
   }
+}
+
+export async function guestLogin(guestid) {
+    try{
+        const response = await fetch(`http://localhost:8080/api/loginguest?guestid=${guestid}`, {
+            method: "POST",
+        });
+
+        if(response.ok){
+            const guestData = await response.json();
+            return guestData;
+            
+          
+        }else{
+            return null;
+        }
+    }catch(e){
+        alert("Error occured "+e);
+    }
 }
