98ed5fabbfa1f275c840882856419cf4f9042b6f
tharana_hasintha <106418791+tharanahasintha@users.noreply.github.com>
2026-01-31 09:13:55 +0530

    update robo URL

--- src/main/java/org/example/projectlibrioo/Service/RobotService/RobotService.java
+++ src/main/java/org/example/projectlibrioo/Service/RobotService/RobotService.java
@@ -4,6 +4,8 @@ import org.example.projectlibrioo.navigation.ShelfPathMap;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 @Service
 public class RobotService {
@@ -14,12 +16,25 @@ public class RobotService {
         this.shelfPathMap = shelfPathMap;
     }
 
-    public void navigateToShelf(int shelfNumber) {
+    /*public void navigateToShelf(int shelfNumber) {
 
         List<String> path = shelfPathMap.getPath(shelfNumber);
         String command = String.join(",", path);
 
         String url = "http://192.168.1.5/move?cmd=" + command;
+        restTemplate.getForObject(url, String.class);
+    }*/
+
+
+
+    public void navigateToShelf(int shelfNumber) {
+
+        List<String> path = shelfPathMap.getPath(shelfNumber);
+        String command = String.join(",", path);
+
+        String encodedCommand = URLEncoder.encode(command, StandardCharsets.UTF_8);
+
+        String url = "http://10.102.165.232/send?num=" + encodedCommand;
         restTemplate.getForObject(url, String.class);
     }
 }
