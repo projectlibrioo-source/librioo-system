dd9796db90115bf6ccdb7ade51102caa688ca97a
nimuthuSW <nimuthusw4@gmail.com>
2026-02-25 17:55:16 +0530

    added sensor reading and stop detection logic to Arduino code

--- arduino_uno.ino
+++ arduino_uno.ino
@@ -73,3 +73,33 @@ void turnRight() {
   digitalWrite(RIGHT_MOTOR_BW, HIGH);  // right reverse
 }
 
+// ================= SENSOR READ =================
+void readSensors(bool &sL2, bool &sL1, bool &sC, bool &sR1, bool &sR2) {
+  sL2 = digitalRead(IR_L2);
+  sL1 = digitalRead(IR_L1);
+  sC  = digitalRead(IR_C);
+  sR1 = digitalRead(IR_R1);
+  sR2 = digitalRead(IR_R2);
+
+  // Debug print
+  Serial.print(sL2); Serial.print(" ");
+  Serial.print(sL1); Serial.print(" ");
+  Serial.print(sC);  Serial.print(" ");
+  Serial.print(sR1); Serial.print(" ");
+  Serial.println(sR2);
+}
+
+// ================= STOP DETECTION =================
+// Your rule: "stops are 4 sensor wide"
+// With 5 sensors, treat a stop as "4 or more sensors detect black".
+bool isStopMarker(bool sL2, bool sL1, bool sC, bool sR1, bool sR2) {
+  // Assumption: LOW = black
+  int blackCount = 0;
+  if (sL2 == LOW) blackCount++;
+  if (sL1 == LOW) blackCount++;
+  if (sC  == LOW) blackCount++;
+  if (sR1 == LOW) blackCount++;
+  if (sR2 == LOW) blackCount++;
+
+  return (blackCount >= 4);
+}
