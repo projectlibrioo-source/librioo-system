a15722058956e46292b701f6def25d43861e0dc2
Pasindu Tharaka <pasindutharaka0707@gmail.com>
2026-03-09 02:36:55 +0530

    add responsive layout for robot assistant display

--- frontend/src/robot/pages/LoginPage.jsx
+++ frontend/src/robot/pages/LoginPage.jsx
@@ -71,6 +71,17 @@ export default function LoginPage() {
           </div>
         </div>
 
+             {/* RIGHT COLUMN: Robot Image */}
+        <div className="relative items-end justify-center hidden w-full h-full lg:flex lg:w-1/2">
+          {/* Holographic glowing orb behind the robot */}
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-cyan-100/30 blur-[100px] rounded-full z-0"></div>
+          
+          <img
+            src={robotImage}
+            alt="Smart Library Assistant Robot"
+            className="relative z-10 scale-125 max-h-[85vh] object-contain drop-shadow-[0_20px_30px_rgba(0,0,0,0.3)] hover:scale-105 transition-transform duration-700 ease-out"
+          />
+
 
 
         </div>
