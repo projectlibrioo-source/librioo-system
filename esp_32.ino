2ccdb75a18999047045c00c9027682e4aa02faaf
sandun80 <sandunsd123@gmail.com>
2026-03-11 09:00:26 +0530

    Configuring web server

--- esp_32.ino
+++ esp_32.ino
@@ -5,3 +5,12 @@
 const char* wifi_ssid = "Sandun";
 const char* wifi_password = "anc12345";
 
+// ===== SERIAL TO UNO =====
+// ESP32 TX2(GPIO19) -> UNO D10 (SoftwareSerial RX)
+// ESP32 GND -> UNO GND
+// (Optional) UNO D11 (TX) -> ESP32 RX2(GPIO16) via level shift
+#define RX2 16
+#define TX2 19
+
+WebServer server(80);
+
