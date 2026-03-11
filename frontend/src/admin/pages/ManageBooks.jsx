e7d3678082d004d0d86739217e3f9818b9201ee3
sandun80 <sandunsd123@gmail.com>
2026-03-11 10:58:50 +0530

    Defining constants

---
+++ frontend/src/admin/pages/ManageBooks.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import AdminLayout from '../layouts/AdminLayout';
+
+const ManageBooks = () => {
+    const [activeTab, setActiveTab] = useState('ADD');
+    const tabs = ['ADD', 'DELETE', 'UPDATE'];
+
+
+};
+
+export default ManageBooks;
