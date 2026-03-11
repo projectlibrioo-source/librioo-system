7b5914bf37fcb8406d2e06d8fdd2ab1e71640651
DinukaD-24 <dinukailangakoon@gmail.com>
2026-03-11 15:59:46 +0530

    Update RobotController with @GetMapping, and test endpoint

--- src/main/java/org/example/projectlibrioo/Controller/Robot/RobotController.java
+++ src/main/java/org/example/projectlibrioo/Controller/Robot/RobotController.java
@@ -3,13 +3,20 @@ package org.example.projectlibrioo.Controller.Robot;
 import org.example.projectlibrioo.Service.RobotService.RobotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RobotController {
+
     @Autowired
     private RobotService robotService;
+
+    @GetMapping("/robot/test")
+    public String test() {
+        return "Robot API is working!";
+    }
 }
