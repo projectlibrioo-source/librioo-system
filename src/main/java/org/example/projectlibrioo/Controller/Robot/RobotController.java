c51921d8d3200de927a6e7dc88abf41d713d7502
DinukaD-24 <dinukailangakoon@gmail.com>
2026-03-12 06:10:04 +0530

    POST /api/robots/add endpoint(add imports)

--- src/main/java/org/example/projectlibrioo/Controller/Robot/RobotController.java
+++ src/main/java/org/example/projectlibrioo/Controller/Robot/RobotController.java
@@ -1,7 +1,12 @@
 package org.example.projectlibrioo.Controller.Robot;
 
+import org.example.projectlibrioo.Model.Robot;
 import org.example.projectlibrioo.Service.RobotService.RobotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
