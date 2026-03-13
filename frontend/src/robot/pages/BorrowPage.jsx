ae1985acfb93e3672e1216cc332718984b641836
Kavindu Jayasinghe <kavindu0356@gmail.com>
2026-03-13 17:41:32 +0530

    create BorrowPage functional component

--- frontend/src/robot/pages/BorrowPage.jsx
+++ frontend/src/robot/pages/BorrowPage.jsx
@@ -2,3 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RobotLayout from "../layouts/RobotLayout";
 import robotImage from "../../assets/pixverse-image-effect-prompt-give-me-three-pic-removebg-preview-1-2.png";
+
+export default function BorrowPage() {
+  
+}
