f7f211117f4457c4a9ebbacd9dd8840db059e3a4
nimuthuSW <nimuthusw4@gmail.com>
2026-03-14 14:53:53 +0530

    feat: add admin routes and pages for login, user management, and settings

--- frontend/src/common/routes/AppRoutes.jsx
+++ frontend/src/common/routes/AppRoutes.jsx
@@ -9,6 +9,10 @@ import SelectionPage from "../../robot/pages/SelectionPage";
 import BorrowPage from "../../robot/pages/BorrowPage";
 import EndingPage from "../../robot/pages/EndingPage";
 import AdminDashboard from "../../admin/pages/AdminDashboard";
+import AdminLogin from "../../admin/pages/AdminLogin";
+import ManageUsers from "../../admin/pages/ManageUsers";
+import ManageBooks from "../../admin/pages/ManageBooks";
+import Settings from "../../admin/pages/Settings";
 
 import GuestLogin from "../../robot/pages/GuestLogin";
 
@@ -29,14 +33,15 @@ function AppRoutes() {
         <Route path="/robot/selection" element={<SelectionPage />} />
         <Route path="/robot/borrow" element={<BorrowPage />} />
         <Route path="/robot/ending" element={<EndingPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-
         <Route path="/robot/guest-login" element={<GuestLogin />} />
-
-        
-        
-        
         <Route path="/robot/search-category" element={<SearchCategory />} />
+        
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/manage-users" element={<ManageUsers />} />
+        <Route path="/admin/manage-books" element={<ManageBooks />} />
+        <Route path="/admin/settings" element={<Settings />} />
+
 
       </Routes>
     </BrowserRouter>
