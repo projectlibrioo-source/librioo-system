8f20ba3a7f8509d59a62ce4ca61439b44eed1de3
DinukaD-24 <dinukailangakoon@gmail.com>
2026-03-17 15:18:33 +0530

    create ReportsService class with @Service annotation

---
+++ src/main/java/org/example/projectlibrioo/Service/Admin/ReportsService.java
@@ -0,0 +1,8 @@
+package org.example.projectlibrioo.Service.Admin;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportsService {
+    // Service methods will be added incrementally
+}
